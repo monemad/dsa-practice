7abffc3dfc7f77dfb50b8605dbb6a4326375697a
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-10-27 21:15:32 -0600

    created the push class method

--- doubly_linked_list.js
+++ doubly_linked_list.js
@@ -12,4 +12,26 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(val) {
+        let node = new Node(val);
+
+        if(!this.length) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length++;
+        return this
+    }
 }
+
+const doublyLinkedList = new DoublyLinkedList();
+
+doublyLinkedList.push("DSA")
+doublyLinkedList.push("rocks")
+doublyLinkedList.push("!")
+console.log(doublyLinkedList);
