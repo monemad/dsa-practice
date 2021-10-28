519a6d09e4005b9618297d3997364d2328db0795
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-10-28 15:29:51 -0600

    implemented queue class and enqueue and dequeue class methods

--- stacks_and_queues.js
+++ stacks_and_queues.js
@@ -1,6 +1,13 @@
 //Stack
 //LIFO
 
+//Time Complexity
+//insertion O(1)
+//removal O(1)
+
+//not really relevant for stacks
+//searching O(N)
+//access O(N)
 class Node{
     constructor(val) {
         this.val = val;
@@ -15,7 +22,7 @@ class Stack {
         this.size = null;
     }
     pop() {
-        if (!this.size) return undefined;
+        if (!this.size) return null;
         const node = this.first;
         if (this.first === this.last)
             this.last = null;
@@ -36,15 +43,58 @@ class Stack {
         }
 
         this.size++;
-        return this;
+        return this.size;
     }
 
 }
 
-const stack = new Stack();
-stack.push('hmmmmmmmm')
-stack.push('one more thing')
-stack.pop()
-console.log(stack)
+// const stack = new Stack();
+// stack.push('hmmmmmmmm')
+// stack.push('one more thing')
+// stack.pop()
+// console.log(stack)
 
+//Time Complexity
+//insertion O(1)
+//removal O(1)
 
+//not really relevant for queues
+//searching O(N)
+//access O(N)
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue (val) {
+        const node = new Node(val)
+        if (!this.size) {
+            this.first = node;
+            this.last = node;
+        } else {
+            this.last.next = node;
+            this.last = node;
+        }
+        this.size++;
+        return this;
+    }
+
+    dequeue() {
+        if (!this.size) return undefined;
+        const node = this.first;
+        if (this.first === this.last)
+            this.last = null;
+        this.first = this.first.next;
+        this.size--;
+        node.next = null;
+        return node;
+    }
+}
+
+// const queue = new Queue()
+// queue.enqueue('bananas')
+// queue.enqueue('mangos')
+// console.log(queue.dequeue())
+// console.log(queue)
