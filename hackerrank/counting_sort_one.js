25c9bec0aab9ec2745bbce3061afb533fdcd9d82
Moiz Ahmad <moiznahmad@gmail.com>
2021-11-09 14:30:50 -0800

    added my sol'n to counting sort 1

--- hackerrank/counting_sort_one.js
+++ hackerrank/counting_sort_one.js
@@ -19,3 +19,12 @@ function countingSort(arr) {
 
     return frequencyArray;
 }
+
+// Moiz
+function countingSort(arr) {
+    // Write your code here
+    let frequencyCounter = new Array(100).fill(0);
+    for (let i = 0; i < arr.length; i ++)
+        frequencyCounter[arr[i]] = frequencyCounter[arr[i]] ? frequencyCounter[arr[i]] + 1 : 1;
+    return frequencyCounter;
+}
